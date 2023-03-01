1a02e5de210c54b1f5f6810599e2a17cc39121c6
Amwatah Obuya <jackalltrade2020@gmail.com>
2023-03-01 16:08:42 +0300

    feature: setup api skelton with tRPC

---
+++ src/server/routers/communites.ts
@@ -0,0 +1,13 @@
+import { router } from "../trpc";
+
+//This end point  contains all procedures (backend functions) related to  communitis
+export const communitesRouter = router({
+  //TODO : Add procedures for  CRUD  operations  @Elijah @Musyoka  @Amwatah
+  //Examples ;
+  // create a new community
+  // get all communities
+  // updated communites
+  // delete  commuinites
+  // join communites
+  // verify communites
+});
