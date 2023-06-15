61a7e3907ea23a08944168ab7cd16c4c3f44457f
Amwatah Obuya <obuyaamwatah@gmail.com>
2023-06-15 17:03:05 +0300

    moved and created pages to app directory

---
+++ src/app/communities/[communityId]/page.tsx
@@ -0,0 +1,8 @@
+
+export default function SingleCommunityPage() {
+    return (
+        <div>
+            Single Community Page
+        </div>
+    )
+}
---
+++ src/app/communities/page.tsx
@@ -0,0 +1,5 @@
+export default function CommunitiesPage() {
+    return (
+        <div>CommunitiesPage</div>
+    )
+}
