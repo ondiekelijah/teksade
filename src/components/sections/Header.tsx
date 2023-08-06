a071e80e63d06ac245e4722d0f3cb13e895af29d
Amwatah Obuya <obuyaamwatah@gmail.com>
2023-08-06 11:33:55 +0300

    fixed all merge conflicts

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -11,36 +11,6 @@ export default function Header() {
   const userStatus = useUser();
 
   return (
-<<<<<<< HEAD
-    <>
-      <header className="sticky inset-x-0 top-0 z-30 px-2 py-2 bg-white shadow-lg dark:bg-slate-900/75 sm:py-3 sm:px-5">
-        <div className="flex items-center justify-between max-w-screen-xl mx-auto">
-          <MenuPopOver
-            className="grow-0 basis-1/3"
-            display="md:hidden"
-            setShowAuth={setShowAuth}
-          />
-          <div className="flex justify-center shrink-0 grow-0 basis-1/3 md:justify-start">
-            <Link href="/#" className="my-auto flex w-[120px] md:ml-0">
-              <Logo />
-            </Link>
-            <div className="hidden gap-2 md:ml-2 md:flex">
-              <Link href="/communities" passHref legacyBehavior>
-                <Button variant="ghost" as="a">
-                  Communities
-                </Button>
-              </Link>
-              <Link href="/about" passHref legacyBehavior>
-                <Button variant="ghost" as="a">
-                  About Us
-                </Button>
-              </Link>
-            </div>
-          </div>
-          <div className="relative flex items-center justify-end gap-2 basis-1/3">
-            {activUser?.email &&
-              <Link href="/#" passHref legacyBehavior>
-=======
     <Paper className=" sticky top-0 z-50  flex w-full items-center justify-between p-2 px-4 shadow-xl">
       <Burger opened={false} className=" text-2xl sm:hidden " onClick={open} />
       <Link href="/" className=" text-3xl font-bold">
@@ -57,7 +27,6 @@ export default function Header() {
             About Us
           </Button>
         </Link>
->>>>>>> feature/communities-page-filters
 
         {userStatus.user ? (
           <Link href="/communities/new">
@@ -97,7 +66,8 @@ export default function Header() {
           transition: "slide-right",
           duration: 300,
           timingFunction: "linear",
-        }}>
+        }}
+      >
         <section className=" flex flex-col gap-y-4">
           <Link href="/communities">
             <Button variant="subtle" className=" rounded-full">
