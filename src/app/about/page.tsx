3d597ddf85e9bd0d30f284453cbf93fc91fb8bea
Cornelius Emase <corneliuslochipi@gmail.com>
2023-08-08 17:19:27 +0300

    added about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 function AboutPage() {
     return (
         <>
-        
                 <div className="container mx-auto py-4">
                     <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
 
