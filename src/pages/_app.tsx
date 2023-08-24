43b844f0ee209ecf6779993468793a355ba15054
Ondiek Elijah Ochieng <oochieng@safaricom.co.ke>
2023-08-24 15:49:20 +0300

    fix: navbar links and theme toggler

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,9 +18,7 @@ function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Teksade</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet"/>
+
       </Head>
 
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
