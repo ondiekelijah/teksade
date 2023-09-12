0169e324ffaa0f9b75ae5292797dcf0d430adfc8
Ondiek Elijah Ochieng <oochieng@safaricom.co.ke>
2023-09-12 10:53:26 +0300

    fix: checkmark not visible

--- src/components/custom-components/icons/checkmark.tsx
+++ src/components/custom-components/icons/checkmark.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 
-interface Props {
-  size?: number;
-}
-
-const Checkmark = ({ size = 4 }: Props) => {
+const Checkmark = () => {
   return (
     <span className={`mr-2 z-50 inline-flex cursor-pointer items-center text-lg font-semibold text-blue-800 dark:text-blue-400 align-middle`} aria-label="Verified">
-      <svg className={`w-${size} h-${size}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+      <svg className={`w-4 h-4`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
         <path
           fill="currentColor"
           d="m18.774 8.245-.892-.893a1.5 1.5 0 0 1-.437-1.052V5.036a2.484 2.484 0 0 0-2.48-2.48H13.7a1.5 1.5 0 0 1-1.052-.438l-.893-.892a2.484 2.484 0 0 0-3.51 0l-.893.892a1.5 1.5 0 0 1-1.052.437H5.036a2.484 2.484 0 0 0-2.48 2.481V6.3a1.5 1.5 0 0 1-.438 1.052l-.892.893a2.484 2.484 0 0 0 0 3.51l.892.893a1.5 1.5 0 0 1 .437 1.052v1.264a2.484 2.484 0 0 0 2.481 2.481H6.3a1.5 1.5 0 0 1 1.052.437l.893.892a2.484 2.484 0 0 0 3.51 0l.893-.892a1.5 1.5 0 0 1 1.052-.437h1.264a2.484 2.484 0 0 0 2.481-2.48V13.7a1.5 1.5 0 0 1 .437-1.052l.892-.893a2.484 2.484 0 0 0 0-3.51Z"
