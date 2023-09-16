aa92b1bf36bab8435193049f338aef63f7b269d8
Ondiek Elijah Ochieng <57575260+ondiekelijah@users.noreply.github.com>
2023-09-16 22:50:25 +0300

    Update CommunityPublishedEmail.tsx

--- src/components/emails/CommunityPublishedEmail.tsx
+++ src/components/emails/CommunityPublishedEmail.tsx
@@ -18,8 +18,7 @@ export const CommunityPublishedEmail = ({ communityName = "Tech community", comm
       <Tailwind>
         <Body className="mx-auto my-auto bg-white font-sans">
           <Container className="mx-auto my-[40px] w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
-            <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">🌍 **Greetings from Teksade!** 🌍</Heading>
-
+            <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">🎈 It&apos;s Official! {communityName} is Now Part of the Teksade Family!</Heading>
             <Text className="text-[14px] leading-[24px] text-black">
               🚀 Sensational News! Your community
               <Link href={`https://www.teksade.com/communities/${communityId}`} className="text-blue-600 no-underline">
