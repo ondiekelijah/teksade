922126c8433059beed7e9a96687446a622ef2c1c
Ondiek Elijah Ochieng <oochieng@safaricom.co.ke>
2023-09-16 23:01:17 +0300

    chore: edit email template

--- src/components/emails/CommunityPublishedEmail.tsx
+++ src/components/emails/CommunityPublishedEmail.tsx
@@ -1,3 +1,4 @@
+import siteMetadata from "@/data/siteMetadata";
 import { Body, Button, Container, Head, Heading, Hr, Html, Img, Link, Preview, Section, Tailwind, Text } from "@react-email/components";
 import * as React from "react";
 
@@ -18,10 +19,13 @@ export const CommunityPublishedEmail = ({ communityName = "Tech community", comm
       <Tailwind>
         <Body className="mx-auto my-auto bg-white font-sans">
           <Container className="mx-auto my-[40px] w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
-            <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">🌍 **Greetings from Teksade!** 🌍</Heading>
+            <div className="text-center">
+              <Img src={`${siteMetadata.siteLogo}`} width={45} height={45} alt="Teksade Logo" />
+            </div>
+            <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">🎈 It&apos;s Official! {communityName} is Now Part of the Teksade Family!</Heading>
 
             <Text className="text-[14px] leading-[24px] text-black">
-              🚀 Sensational News! Your community
+              🚀 Sensational News! Your community{" "}
               <Link href={`https://www.teksade.com/communities/${communityId}`} className="text-blue-600 no-underline">
                 {communityName}
               </Link>
@@ -33,7 +37,7 @@ export const CommunityPublishedEmail = ({ communityName = "Tech community", comm
               away. 🤝
             </Text>
 
-            <Section className="mb-[32px] mt-[32px] text-center">
+            <Section className="text-center">
               <Button pX={20} pY={12} className="rounded-full bg-[#5c7cfa] text-center text-[12px] font-semibold text-white no-underline" href={`https://www.teksade.com/communities/${communityId}`}>
                 Check it out
               </Button>
