210076da412cb2d4755f697f95d2d4f1b5da66e0
Amwatah Obuya <obuyaamwatah@gmail.com>
2023-09-22 13:50:06 +0300

    removed auth middware,added auth checks

--- src/pages/admin/publish.tsx
+++ src/pages/admin/publish.tsx
@@ -2,6 +2,7 @@ import { PageSEO } from "@/components/SEO";
 import CustomButton from "@/components/custom-components/button";
 import Container from "@/components/custom-components/container";
 import SectionTitle from "@/components/custom-components/sectionTitle";
+import ProtectedPageLayout from "@/components/layouts/ProtectedPageLayout";
 import CommmunityCard from "@/components/sections/CommmunityCard";
 import siteMetadata from "@/data/siteMetadata";
 import useAdminCheck from "@/hooks/useAuth";
@@ -19,37 +20,39 @@ export default function PublishCommunityPage() {
   function handlePublish(communityId: string) {
     publishAndSendEmail.mutate({
       communityId,
-    })
+    });
   }
   return (
-    <Container>
-      <PageSEO title={"Admin"} description={siteMetadata.community_description} />
-      <SectionTitle heading="Admin Panel" description="Publish Communities" />
-      <section className=" grid grid-cols-1 gap-2 sm:grid-cols-3">
-        {user?.primaryEmailAddress?.emailAddress && userIsAdmin ? (
-          unpulblishedCommunities.data?.map((community) => (
-            <CommmunityCard
-              key={community.name}
-              id={community.id}
-              description={community.description}
-              location={community.location}
-              country={community.country}
-              name={community.name}
-              members={0}
-              verified={community.verified ? true : false}
-              logoUrl={community.logo_link}
-              actionButtons={
-                <div className="flex w-full items-center justify-between">
-                  <CustomButton size="sm" variant="filled" title="Revoke" color="red" />
-                  <CustomButton size="sm" variant="filled" title="Publish" onClickHandler={() => handlePublish(community.id)} />
-                </div>
-              }
-            />
-          ))
-        ) : (
-          <div className="text-center sm:col-span-3">{`Oops! You don't have permission to view this.`}</div>
-        )}
-      </section>
-    </Container>
+    <ProtectedPageLayout>
+      <Container>
+        <PageSEO title={"Admin"} description={siteMetadata.community_description} />
+        <SectionTitle heading="Admin Panel" description="Publish Communities" />
+        <section className=" grid grid-cols-1 gap-2 sm:grid-cols-3">
+          {user?.primaryEmailAddress?.emailAddress && userIsAdmin ? (
+            unpulblishedCommunities.data?.map((community) => (
+              <CommmunityCard
+                key={community.name}
+                id={community.id}
+                description={community.description}
+                location={community.location}
+                country={community.country}
+                name={community.name}
+                members={0}
+                verified={community.verified ? true : false}
+                logoUrl={community.logo_link}
+                actionButtons={
+                  <div className="flex w-full items-center justify-between">
+                    <CustomButton size="sm" variant="filled" title="Revoke" color="red" />
+                    <CustomButton size="sm" variant="filled" title="Publish" onClickHandler={() => handlePublish(community.id)} />
+                  </div>
+                }
+              />
+            ))
+          ) : (
+            <div className="text-center sm:col-span-3">{`Oops! You don't have permission to view this.`}</div>
+          )}
+        </section>
+      </Container>
+    </ProtectedPageLayout>
   );
 }
--- src/pages/communities/created.tsx
+++ src/pages/communities/created.tsx
@@ -1,12 +1,13 @@
 import { PageSEO } from "@/components/SEO";
+import ProtectedPageLayout from "@/components/layouts/ProtectedPageLayout";
 import CreatedCommunitiesPage from "@/components/pages/CreatedCommunities";
 import siteMetadata from "@/data/siteMetadata";
 
 export default function Created() {
   return (
-    <>
+    <ProtectedPageLayout>
       <PageSEO title={"Manage Communities"} description={siteMetadata.add_community_description} />
       <CreatedCommunitiesPage />
-    </>
+    </ProtectedPageLayout>
   );
 }
