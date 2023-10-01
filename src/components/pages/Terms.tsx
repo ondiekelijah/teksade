5efc4542cf4976ee0e0fb9e5d337c6ae06d78dc4
Ondiek Elijah Ochieng <oochieng@safaricom.co.ke>
2023-10-01 15:32:02 +0300

    refac: terms page following DRY principles

--- src/components/pages/Terms.tsx
+++ src/components/pages/Terms.tsx
@@ -1,174 +1,78 @@
+import { useMantineColorScheme } from "@mantine/core";
 import Container from "../custom-components/container";
+import siteMetadata from "@/data/siteMetadata";
+
+interface SectionProps {
+  title: string;
+  subSections: SubSectionProps[];
+  dark?: boolean;
+}
+
+interface SubSectionProps {
+  title: string;
+  content: string | JSX.Element;
+  dark?: boolean;
+}
+
+const Section = ({ title, subSections, dark }: SectionProps) => {
+  return (
+    <div className="mb-8">
+      <h2 className="mb-4 text-2xl font-bold">{title}</h2>
+      {subSections?.map((subSection, index) => (
+        <SubSection key={index} title={subSection.title} content={subSection.content} dark={dark} />
+      ))}
+    </div>
+  );
+};
+
+const SubSection = ({ title, content, dark }: SubSectionProps) => {
+  return (
+    <div className="mb-4">
+      <h3 className="mb-2 text-xl font-bold">{title}</h3>
+      <p className={`${dark ? "text-slate-400" : "text-slate-600"}`}>{content}</p>
+    </div>
+  );
+};
 
 export default function Terms() {
+  const { colorScheme } = useMantineColorScheme();
+  const dark = colorScheme === "dark";
+
   return (
     <Container className="">
-      <div className=" mx-auto flex flex-col gap-y-4 p-8">
-        <h1 className="mb-8 text-4xl font-bold">Welcome to Teksade!</h1>
-        <p>
+      <div className=" mx-auto flex flex-col gap-y-4 py-8">
+        <h1 className="mb-2 text-4xl font-bold">Welcome to Teksade!</h1>
+        <p className={`${dark ? "text-slate-400" : "text-slate-600"}`}>
           Welcome to Teksade, a community-driven platform connecting tech enthusiasts! This Privacy Policy is designed to help you understand how we collect, use, and safeguard your personal
           information. By using Teksade, you agree to the terms outlined in this policy.
         </p>
+        {siteMetadata?.termsData.map((section, index) => (
+          <Section key={index} title={section.title} subSections={section.subSections} dark={dark} />
+        ))}
 
-        <div className="mb-8">
-          <h2 className="mb-4 text-2xl font-bold">1. Terms of Service</h2>
-          <div className="mb-4">
-            <h3 className="mb-2 text-xl font-bold">1.1 Acceptance of Terms</h3>
-            <p>By accessing or using our platform, you agree to these Terms of Service. These terms govern your use of Teksade, including all features and functionalities.</p>
-          </div>
+        {/* Contact Us Section */}
+        <Section
+          title="Contact Us"
+          subSections={[
+            {
+              title: "Contact Information",
+              content: (
+                <span className={`${dark ? "text-slate-400" : "text-slate-600"}`}>
+                  If you have any questions about our Terms of Service, please contact us at{" "}
+                  <a href="mailto:contact@teksade.com" className="text-blue-500 hover:underline">
+                    contact@teksade.com
+                  </a>
+                  .
+                </span>
+              ),
+            },
+          ]}
+        />
 
-          <div className="mb-4">
-            <h3 className="mb-2 text-xl font-bold">1.2 Modification of Terms</h3>
-            <p>
-              We reserve the right to modify these terms at any time. Changes will be effective immediately upon posting. Your continued use of Teksade after changes constitutes your acceptance of the
-              updated terms. We will make our best efforts to notify users of significant changes.
-            </p>
-          </div>
-
-          <div className="mb-4">
-            <h3 className="mb-2 text-xl font-bold">1.3 User Eligibility</h3>
-            <p>
-              You must be 18 years or older to use Teksade. By using our platform, you confirm that you meet this requirement. If you are under 18, you may use Teksade only with the involvement of a
-              parent or guardian.
-            </p>
-          </div>
-
-          <div className="mb-4">
-            <h3 className="mb-2 text-xl font-bold">1.4 User Account</h3>
-            <p>
-              You are responsible for maintaining the confidentiality of your account and password. You agree to notify us immediately of any unauthorized access or use of your account. We will never
-              ask you for your password via email or any other unsolicited communication.
-            </p>
-          </div>
-
-          <div className="mb-4">
-            <h3 className="mb-2 text-xl font-bold">1.5 Community Guidelines</h3>
-            <p>
-              You agree to adhere to our community guidelines, which prohibit hate speech, harassment, illegal activities, and any form of abusive behavior. Users found violating these guidelines may
-              face account suspension or termination.
-            </p>
-          </div>
-
-          <div className="mb-4">
-            <h3 className="mb-2 text-xl font-bold">1.6 Termination</h3>
-            <p>
-              We reserve the right to terminate or suspend your account at our sole discretion, without prior notice, for conduct that we believe violates these terms. Upon termination, your right to
-              use Teksade will immediately cease.
-            </p>
-          </div>
-        </div>
-
-        <div className="mb-8">
-          <h2 className="mb-4 text-2xl font-bold">2. User Data and Privacy</h2>
-          <div className="mb-4">
-            <h3 className="mb-2 text-xl font-bold">2.1 Data Collection</h3>
-            <p>
-              We collect information you provide, such as name, email, location, and social links, to create and manage your Teksade account. Additionally, we may collect data about your interactions
-              with our platform, such as the communities you join and the content you post.
-            </p>
-          </div>
-
-          <div className="mb-4">
-            <h3 className="mb-2 text-xl font-bold">2.2 Data Usage</h3>
-            <p>
-              We use your data to personalize your experience, improve our services, and communicate with you. Your data helps us recommend relevant communities and content. We do not sell your data
-              to third parties. For a detailed overview of how we use and protect your data, please refer to our Privacy Policy.
-            </p>
-          </div>
-
-          <div className="mb-4">
-            <h3 className="mb-2 text-xl font-bold">2.3 Data Security</h3>
-            <p>
-              We employ industry-standard security measures to protect your data from unauthorized access, alteration, disclosure, or destruction. These measures include encryption, access controls,
-              and regular security audits.
-            </p>
-          </div>
-
-          <div className="mb-4">
-            <h3 className="mb-2 text-xl font-bold">2.4 Cookies</h3>
-            <p>
-              We use cookies and similar technologies to enhance your experience on Teksade. Cookies are small text files stored on your device that help us track user behavior and improve our
-              services. By using Teksade, you consent to our use of cookies in accordance with our Cookie Policy.
-            </p>
-          </div>
-        </div>
-
-        <div className="mb-8">
-          <h2 className="mb-4 text-2xl font-bold">3. License and Copyright</h2>
-          <div className="mb-4">
-            <h3 className="mb-2 text-xl font-bold">3.1 Content Ownership</h3>
-            <p>
-              You retain ownership of the content you submit. By posting, you grant us a worldwide, non-exclusive, royalty-free license to use, reproduce, modify, publish, distribute, and display the
-              content. This license allows us to operate and improve Teksade, promote your content, and share it with other users.
-            </p>
-          </div>
-
-          <div className="mb-4">
-            <h3 className="mb-2 text-xl font-bold">3.2 Copyright Infringement</h3>
-            <p>
-              If you believe your copyright-protected work is displayed on Teksade without authorization, please notify us with detailed information, and we will take appropriate action. This may
-              include content removal and, in some cases, the suspension or termination of the user responsible for the infringement.
-            </p>
-          </div>
-        </div>
-
-        <div className="mb-8">
-          <h2 className="mb-4 text-2xl font-bold">4. Policies</h2>
-          <div className="mb-4">
-            <h3 className="mb-2 text-xl font-bold">4.1 Community Creation</h3>
-            <p>
-              Community leaders are responsible for the accuracy and legality of the information provided. Misleading or inappropriate content will be removed. Community leaders must adhere to our
-              guidelines and agree not to create communities that promote hate speech, violence, or illegal activities.
-            </p>
-          </div>
-
-          <div className="mb-4">
-            <h3 className="mb-2 text-xl font-bold">4.2 User Conduct</h3>
-            <p>
-              Users must respect the guidelines and rules of each community. Harassment, trolling, or any form of abusive behavior will result in account suspension. Users are encouraged to report any
-              violations they encounter.
-            </p>
-          </div>
-
-          <div className="mb-4">
-            <h3 className="mb-2 text-xl font-bold">4.3 Disclaimer of Warranties</h3>
-            <p>
-              Teksade is provided &quot;as is&quot; without warranties of any kind, either express or implied. We do not guarantee the accuracy, reliability, or availability of the platform. While we
-              strive to provide a secure and reliable service, we cannot guarantee that Teksade will be uninterrupted or error-free.
-            </p>
-          </div>
-
-          <div className="mb-4">
-            <h3 className="mb-2 text-xl font-bold">4.4 Limitation of Liability</h3>
-            <p>
-              We shall not be liable for any indirect, incidental, special, consequential, or punitive damages arising out of or in connection with your use of Teksade. In no event shall our liability
-              exceed the amount paid by you (if any) for accessing Teksade.
-            </p>
-          </div>
-
-          <div className="mb-4">
-            <h3 className="mb-2 text-xl font-bold">4.5 Governing Law</h3>
-            <p>
-              These terms are governed by and construed in accordance with the laws of [Your Jurisdiction]. Any legal action or proceeding relating to your access to, or use of, Teksade shall be
-              instituted in a court in [Your Jurisdiction], and you agree to submit to the jurisdiction of such court.
-            </p>
-          </div>
-        </div>
-
-        <div className="mb-8">
-          <h2 className="mb-4 text-2xl font-bold">Contact Us</h2>
-          <p>
-            If you have any questions about our Terms of Service, please contact us at{" "}
-            <a href="mailto:contact@teksade.com" className="text-blue-500 hover:underline">
-              contact@teksade.com
-            </a>
-            .
-          </p>
-
-          <p className="mt-2">
-            <strong>Last Updated:</strong> [01/10/2023]
-          </p>
-        </div>
+        {/* Last Updated */}
+        <p className={`${dark ? "text-slate-400" : "text-slate-600"}`}>
+          <strong>Last Updated:</strong> [01/10/2023]
+        </p>
       </div>
     </Container>
   );
