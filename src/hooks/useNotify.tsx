037bb86432f36b59798428b536d2c47c4fc5e300
Amwatah Obuya <obuyaamwatah@gmail.com>
2023-10-18 18:50:49 +0300

    migrated hooks

---
+++ src/hooks/useNotify.tsx
@@ -0,0 +1,40 @@
+import {
+  ErrorIcon,
+  SuccessIcon,
+} from "@/app/_components/custom-components/icons/actionIcons";
+import { showNotification } from "@mantine/notifications";
+
+interface NotificationOptions {
+  title?: string;
+  message: string;
+}
+
+const useMantineNotify = () => {
+  const notifySuccess = ({
+    message,
+    title = "Success",
+  }: NotificationOptions) => {
+    showNotification({
+      title,
+      message,
+      color: "green",
+      icon: <SuccessIcon />,
+    });
+  };
+
+  const notifyError = ({
+    message,
+    title = "That didn't work",
+  }: NotificationOptions) => {
+    showNotification({
+      title,
+      message,
+      color: "red",
+      icon: <ErrorIcon />,
+    });
+  };
+
+  return { notifySuccess, notifyError };
+};
+
+export default useMantineNotify;
