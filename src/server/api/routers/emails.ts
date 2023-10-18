8dac6b80cdd1be67bf953cd101ccef3bc646674a
Amwatah Obuya <obuyaamwatah@gmail.com>
2023-10-18 18:27:17 +0300

    migrated api endpoints routers

---
+++ src/server/api/routers/emails.ts
@@ -0,0 +1,51 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "../trpc";
+import { Resend } from "resend";
+import CommunityPublishedEmail from "@/app/_components/emails/community-published-email";
+const resend = new Resend(process.env.RESEND_EMAIL_API_KEY);
+export const emailsRouter = createTRPCRouter({
+  publishAndsendCommunityPublishedEmail: publicProcedure
+    .input(
+      z.object({
+        communityId: z.string(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      try {
+        const communityToPublish = await ctx.db.community.update({
+          where: {
+            id: input.communityId,
+          },
+          data: {
+            published: true,
+          },
+          select: {
+            name: true,
+            id: true,
+            creator: {
+              select: {
+                email: true,
+              },
+            },
+          },
+        });
+        const sendCommunityPublishedEmail = await resend.emails.send({
+          from: "Teksade <contact@teksade.com>",
+          to: communityToPublish.creator.email ?? "teksadeproject@gmail.com",
+          subject: "🎉 Hooray! Your community is now live!",
+          react: CommunityPublishedEmail({
+            communityName: communityToPublish.name,
+            communityId: communityToPublish.id,
+          }),
+        });
+        if (sendCommunityPublishedEmail.id) {
+          console.log("Sent to", communityToPublish.creator.email);
+        } else {
+          console.log(" Not Sent", communityToPublish.creator.email);
+        }
+        return sendCommunityPublishedEmail.id;
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+});
---
+++ src/server/api/routers/likes.ts
@@ -0,0 +1,80 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const likesRouter = createTRPCRouter({
+  addLikeToCommunity: publicProcedure
+    .input(
+      z.object({
+        memberId: z.string(),
+        communityId: z.string(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      try {
+        const newLike = await ctx.db.like.create({
+          data: {
+            communityId: input.communityId,
+            memberId: input.memberId,
+          },
+        });
+
+        if (newLike) {
+          return true;
+        } else {
+          return false;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+
+  getCommunintyLikes: publicProcedure
+    .input(
+      z.object({
+        communityId: z.string(),
+      }),
+    )
+    .query(async ({ input, ctx }) => {
+      try {
+        const getNumberOfLikes = await ctx.db.community.findUnique({
+          where: {
+            id: input.communityId,
+          },
+          select: {
+            likes: {
+              select: {
+                id: true,
+                memberId: true,
+              },
+            },
+            _count: {
+              select: {
+                likes: true,
+              },
+            },
+          },
+        });
+        return getNumberOfLikes;
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+  removeExistingLike: publicProcedure
+    .input(
+      z.object({
+        likeId: z.number(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      try {
+        const deletedLike = await ctx.db.like.delete({
+          where: {
+            id: input.likeId,
+          },
+        });
+        return deletedLike;
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+});
