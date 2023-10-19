5a3d68cdef7d278305df2f0d995b362dd26edbe1
Amwatah Obuya <obuyaamwatah@gmail.com>
2023-10-19 12:45:46 +0300

    fixed  layout render mismatch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,12 @@ import "@mantine/carousel/styles.css";
 import "@/styles/globals.css";
 
 import { Inter } from "next/font/google";
-
+import { headers } from "next/headers";
 import { ColorSchemeScript } from "@mantine/core";
 import MainProvider from "./_components/providers/main-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import MainLayout from "./_components/layouts/MainLayout";
+import { TRPCReactProvider } from "@/trpc/react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -28,23 +29,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-      appearance={{
-        variables: {
-          colorPrimary: "#1A56DB",
-        },
-      }}
-    >
-      <html lang="en">
-        <head>
-          <ColorSchemeScript />
-        </head>
-        <body className={`font-sans ${inter.variable}`}>
-          <MainProvider>
-            <MainLayout>{children}</MainLayout>
-          </MainProvider>
-        </body>
-      </html>
-    </ClerkProvider>
+    <TRPCReactProvider headers={headers()}>
+      <ClerkProvider
+        appearance={{
+          variables: {
+            colorPrimary: "#1A56DB",
+          },
+        }}
+      >
+        <html lang="en">
+          <head>
+            <ColorSchemeScript />
+          </head>
+          <body className={`font-sans ${inter.variable}`}>
+            <MainProvider>
+              <MainLayout>{children}</MainLayout>
+            </MainProvider>
+          </body>
+        </html>
+      </ClerkProvider>
+    </TRPCReactProvider>
   );
 }
