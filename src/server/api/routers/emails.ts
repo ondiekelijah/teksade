2174545531c580112e460ca61b0fd9d4d6199e5f
Amwatah Obuya <obuyaamwatah@gmail.com>
2024-02-23 12:30:01 +0300

    re-implemented Resend emails with new version

--- src/server/api/routers/emails.ts
+++ src/server/api/routers/emails.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
-
+import { Resend } from "resend";
+import CommunityPublishedEmail from "@/app/_components/emails/community-published-email";
+const resend = new Resend(process.env.RESEND_EMAIL_API_KEY);
 export const emailsRouter = createTRPCRouter({
   publishAndsendCommunityPublishedEmail: publicProcedure
     .input(
@@ -26,7 +28,20 @@ export const emailsRouter = createTRPCRouter({
           },
         },
       });
-
-      return communityToPublish;
+      const sendCommunityPublishedEmail = await resend.emails.send({
+        from: "Teksade <contact@teksade.com>",
+        to: communityToPublish.creator.email ?? "teksadeproject@gmail.com",
+        subject: "🎉 Hooray! Your community is now live!",
+        react: CommunityPublishedEmail({
+          communityName: communityToPublish.name,
+          communityId: communityToPublish.id,
+        }),
+      });
+      if (sendCommunityPublishedEmail.data?.id) {
+        console.log("Sent to", communityToPublish.creator.email);
+      } else {
+        console.log(" Not Sent", communityToPublish.creator.email);
+      }
+      return sendCommunityPublishedEmail.data?.id;
     }),
 });
